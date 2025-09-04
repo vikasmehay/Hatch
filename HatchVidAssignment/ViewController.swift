8c2152826351e1ba3cfce3ca14bdaf6727094c8b
Vikas <vikas@ikoble.com>
2025-09-04 00:05:43 -0400

    Initial Commit

---
+++ HatchVidAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HatchVidAssignment
+//
+//  Created by Vikas Mehay on 2025-09-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
